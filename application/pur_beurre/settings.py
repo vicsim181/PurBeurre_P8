0242a3fe448d57c13a2acf42ecbaaf9a0adf4dac
vicsim181 <63299151+vicsim181@users.noreply.github.com>
2021-02-24 17:35:54 +0100

    creation of the 'main' application
    Creation of the "Main" application in charge of managing the homepage, displaying the research results

--- application/pur_beurre/settings.py
+++ application/pur_beurre/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'pur_beurre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pur_beurre',
+        'USER': 'postgres',
+        'PASSWORD': os.environ['POSTGRESQL'],
+        'HOST': '',
+        'PORT': '5432',
     }
 }
 
