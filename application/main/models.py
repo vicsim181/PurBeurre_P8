26aedc7dfbaf6d643c501fe3b17bad878a65b4c2
vicsim181 <63299151+vicsim181@users.noreply.github.com>
2021-02-25 14:32:18 +0100

    commit pre session
    Commit before session of 25/02

--- application/main/models.py
+++ application/main/models.py
@@ -1,39 +1,3 @@
-import datetime
-
 from django.db import models
-from django.db.models.deletion import CASCADE
-from django.utils import timezone
 
 # Create your models here.
-
-
-class Store(models.Model):
-    name = models.CharField(max_length=100)
-
-
-class User(models.Model):
-    name = models.CharField(max_length=50)
-    username = models.CharField(max_length=10, unique=True)
-    email = models.EmailField(max_length=30, unique=True)
-    hash_password = models.CharField(max_length=90)
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-    parent_id = models.ForeignKey(Category, on_delete=models.CASCADE)
-
-
-class Product(models.Model):
-    name = models.CharField(max_length=100)
-    id_category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    nutriscore = models.CharField(max_length=1)
-    description = models.CharField(max_length=200)
-    url = models.URLField
-    store = models.ManyToManyField(Store, null=True)
-
-
-class Favorite(models.Model):
-    id_replaced_product = models.ForeignKey(Product, on_delete=CASCADE)
-    id_replacing_product = models.ForeignKey(Product, on_delete=CASCADE)
-    date_creation = models.DateTimeField(default=timezone.now())
-    user_id = models.ForeignKey(User, on_delete=CASCADE)
