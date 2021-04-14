dbc6998e831111bbdd93aacb3b8c0c4106390d17
vicsim181 <63299151+vicsim181@users.noreply.github.com>
2021-04-14 18:44:57 +0200

    Update settings.py

--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -33,9 +33,9 @@ ALLOWED_HOSTS = ['.herokuapps.com', 'localhost', '127.0.0.1:8000']
 # Application definition
 
 INSTALLED_APPS = [
-    'main.apps.MainConfig',
-    'bookmark.apps.BookmarkConfig',
-    'authentication.apps.AuthenticationConfig',
+    'application.main',
+    'application.bookmark',
+    'application.authentication',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
